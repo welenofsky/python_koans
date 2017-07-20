0758a7d442f14e55b1d626e67aa06ab7efd70fa6
Justin Welenofsky <welenofsky@gmail.com>
2017-07-20 14:33:27 -0700

    about_triangle_project2

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,13 +17,27 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    set_len = len({a,b,c})
+    # Sort so I know where the big nums and small nums are
+    sides = sorted([a,b,c])
+    # Helps identify how many sides are the same length
+    set_len = len(set(sides))
+
+    if (sides[0] < 1):
+        # If any of the sides are less than 1
+        raise TriangleError
+    elif ((sides[0] + sides[1]) < sides[2]):
+        # If not valid triangle
+        raise TriangleError
+    
+    # Identify Triangle Type
     if (set_len == 1):
         return 'equilateral'
     elif (set_len == 2):
         return 'isosceles'
     elif (set_len == 3):
         return 'scalene'
+
+    return ''
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
